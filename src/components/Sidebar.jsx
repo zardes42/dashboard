0af2da70494650d3ad5c6c7f25dabe77f8ac2eec
Iyinoluwa <iyinoyelz69@gmail.com>
2024-02-24 04:58:52 -0700

    fixes sidebar not closin

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,16 +6,18 @@ import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
-import { classNames } from "@syncfusion/ej2/buttons";
 import { useStateContext } from "../contexts/ContextProvider";
 
 const Sidebar = () => {
-  const {activeMenu,setActiveMenu} = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+  const handleCloseSideBar = () => {
+    if(setActiveMenu && screenSize <=900)setActiveMenu(false);
+    
+  };
   const activeLink =
     "flex items-cente gap-5 pl-4 p4-3 pb-2.5 rounded-lg text-white text-md m-2";
   const normalLink =
     "flex items-center gap-5 pl-4 p4-3 pb-2.5 rounded-lg  text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2 ";
-  
 
   return (
     <div className="ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10 ">
@@ -24,7 +26,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware />
@@ -48,7 +50,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+                    onClick={handleCloseSideBar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
